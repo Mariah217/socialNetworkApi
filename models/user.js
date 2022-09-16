2f137cf7a5e7464580f2dcfb611a2dc361304bf7
Mariah <mariahjohnston217@outlook.com>
2022-09-15 21:35:58 -0400

    completed user model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,49 @@
-const {Schema, model} = require('mongoose')
+const {Schema, model} = require('mongoose');
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
 
 const userSchema = new Schema (
     {
-        username: 
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, 'Please fill a valid email address'],
+            match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+        }, 
+        thoughts: {
+            type: Schema.type,
+            ref: 'thought',
+        }, 
+        friends: {
+            type: Schema.Type,
+            ref: 'user',
+        },
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+    
+);
+
+userSchema
+.virtual('friendCount')
+.get(function () {
+    return this.friends.length;
+});
+
+const User = model('user', userSchema);
+
+module.exports = User;
