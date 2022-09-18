8ef22d57a7bfb222e1ac5baac791e15b0adf8d2c
Mariah <mariahjohnston217@outlook.com>
2022-09-17 21:54:33 -0400

    completed thoughtRoutes file

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const {
+    getAllThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    removeReaction,
+} = require('../../controllers/thoughtController');
+
+// /api/thought
+router.route('/').get(getAllThoughts).post(createThought);
+
+// /api/thought/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought).update(updateThought)
+
+// /api/thought/:thoughtId/reactions
+router.route('/:reactionId/reactions').post(addReaction).delete(removeReaction);
+
+module.exports = router;
