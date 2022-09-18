a807a2c2019f8d47627e01a43f7dc48342d0be7f
Mariah <mariahjohnston217@outlook.com>
2022-09-17 21:44:03 -0400

    completed userRoutes file

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+const {
+  getAllUsers,
+  getSingleUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+} = require('../../controllers/userController');
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser);
+router.route('/:userId').delete(deleteUser);
+router.route('/:userId').put(updateUser);
+
+// /api/users/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(addFriend)
+router.route('/:userId/friends/:friendId').delete(removeFriend)
+
+module.exports = router;
